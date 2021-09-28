496db77b899af038d26829117efc2e7edad0ab93
Bart PC <bbart318@gmail.com>
2021-09-27 19:35:01 -0500

    Remove result variable from operate function

--- app.js
+++ app.js
@@ -4,12 +4,10 @@ const operator = {
   "×": (a, b) => a * b,
   "/": (a, b) => a / b,
 };
-let result;
 
 //operate
 const operate = (operator, a, b) => {
-  result = operator(a, b);
-  return result;
+  return operator(a, b);
 };
 //positve/negative
 const posNeg = (a) => {
